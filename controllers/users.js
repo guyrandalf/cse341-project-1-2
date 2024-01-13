0c64f0e263627cec3966b1b7ae6595837aeac3d5
Randalf Ehigaitor <randalfosabuohien@gmail.com>
2024-01-13 16:08:25 +0100

    separate controllers for both routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
 const mongodb = require("../database");
-const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
   const result = await mongodb
@@ -13,34 +12,4 @@ const getAll = async (req, res) => {
   });
 };
 
-const getSingle = async (req, res) => {
-  const userId = req.params.id;    
-  if (!ObjectId.isValid(userId)) {
-    return res.status(400).json({ error: "Invalid user ID" });
-  }
-
-  try {
-    const result = await mongodb
-      .getDatabase()
-      .db("csewk1-2")
-      .collection("users")
-      .find({ _id: new ObjectId(userId) });
-
-    const users = await result.toArray();
-
-    if (users.length === 0) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(users[0]);
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-module.exports = {
-  getAll,
-  getSingle,
-};
+module.exports = getAll
